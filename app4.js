d6c6c8855e25ebb9b66142b83657280086abf57d
EG12385 <61690459+EG12385@users.noreply.github.com>
2022-02-24 17:04:06 +0200

    Business Coordinators

---
+++ app4.js
@@ -0,0 +1,60 @@
+
+//function checkUnique(elementID)
+//{
+//funtion to check unique values
+
+//var elt = document.getElementById(elementID);
+//var playsubcode =document.getElementByID(elementID).value;
+//var elementIDsuffix=parseInt(elementID.substring(3))-1;
+
+//}
+
+// get one array to stored all select option values
+
+//var othercodes = [
+//  document.getElementByID("play1").value,
+//    document.getElementByID("play2").value,
+//      document.getElementByID("play3").value,
+  //      document.getElementByID("go").value,
+//];
+
+//for (var i=0 ; 1<= 2; i++){
+
+  // check all dropdown
+
+//  if (i != elementIDsuffix) {
+
+//  if ((elementId.id == "play1" && "one") ||
+       //(elementId.id== "play2" && "1dice")||
+       //(elementId.id == "play3" && "total"))
+
+//{
+// document.getElementByID("go") href="game9.html";
+//}
+
+
+//      else {
+//   href ="game4.html"
+  //    }
+
+//    }
+//  }
+
+function testPlay() {
+
+  document.getElementById("go").onclickfunction
+
+  var users = document.getElementById("users");
+  var roll = document.getElementById("roll");
+  var count = document.getElementById("count");
+
+  if ((container.id == users && dropLog == "one") ||
+       (container.id == roll && dropLog == "1dice")||
+       (container.id== count && dropLog == "total"))
+
+{
+ href="game9.html";
+}
+
+
+}
